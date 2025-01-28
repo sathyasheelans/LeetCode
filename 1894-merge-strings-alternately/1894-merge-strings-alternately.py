c0d145aeb2133a57727a8796b8899e3f15dcd6ca
Sathyasheelan Santhanam <77305585+sathyasheelans@users.noreply.github.com>
2025-01-28 01:00:00 -0500

    Time: 34 ms (78.23%), Space: 17.6 MB (53.49%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -7,13 +7,7 @@ class Solution:
         for w1, w2 in zip(word1, word2):
             result.append(w1)
             result.append(w2)
-        
-        if l1>l2:
-            result.append(word1[len(word2):])
-        else:
-            result.append(word2[len(word1):])
-
 
         
-        return "".join(result)
+        return "".join(result + [word1[len(word2):]] + [word2[len(word1):]])
             
