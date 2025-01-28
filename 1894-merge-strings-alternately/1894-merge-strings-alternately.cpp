d9e0c53aa44e777d342058d5cd6be3990180a778
Sathyasheelan Santhanam <77305585+sathyasheelans@users.noreply.github.com>
2025-01-28 00:55:14 -0500

    Time: 0 ms (100%), Space: 8.3 MB (83.45%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string merge;
-        merge.reserve(word1.length() + word2.length());
-
+        string merge="";
+        
         for(int i=0; i<max(word1.length(),word2.length());i++){
             if(i<word1.length())
             {
-                merge += word1[i];
+                merge.push_back(word1[i]);
             }
             if(i<word2.length()){
-                merge += word2[i];
+                merge.push_back(word2[i]);
             }
             
         }
