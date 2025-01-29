d0818ad71b7eb2d01c08f6c064adc86465d70afd
Sathyasheelan Santhanam <77305585+sathyasheelans@users.noreply.github.com>
2025-01-29 01:13:42 -0500

    Time: 0 ms (100%), Space: 9.8 MB (32.02%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
         
-        if(str1+str2 != str2+str1){
-            return "";
-        }
-        return str1.substr(0,gcd(str1.length(),str2.length()));
+        return (str1 + str2 == str2 + str1)? 
+        str1.substr(0, gcd(size(str1),size(str2))): "";
         
     }
 };
